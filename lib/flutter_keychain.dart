4c271e4a4d2875b3fc62f9a48892f15b0d33501b
Najdan Tomić <najdan.tomic@gmail.com>
2025-08-21 17:08:30 +0200

    Allow optional values for put

--- lib/flutter_keychain.dart
+++ lib/flutter_keychain.dart
@@ -6,7 +6,13 @@ class FlutterKeychain {
   static const MethodChannel _channel = const MethodChannel('plugin.appmire.be/flutter_keychain');
 
   // put - store the value for a key
-  static Future<void> put({required String key, required String value}) async => _channel.invokeMethod('put', {'key': key, 'value': value});
+  static Future<void> put({required String key, required String? value}) async {
+    if (value != null) {
+      _channel.invokeMethod('put', {'key': key, 'value': value});
+    } else {
+      remove(key: key);
+    }
+  }
 
   // get - get the value for a given key
   static Future<String?> get({required String key}) async => await _channel.invokeMethod('get', {'key': key});
